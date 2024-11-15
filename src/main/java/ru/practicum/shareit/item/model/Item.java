4373172d86b7985dc98062d9b944b8dd016e359c
Maxim Skoblenkov <goofy.net@yandex.ru>
2024-11-15 19:52:08 +0400

    add-controller: ТЗ-14

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.model;
 
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,7 +10,6 @@ import lombok.NoArgsConstructor;
 public class Item {
     private Long id; // id вещи
     private String name; // краткое название вещи
-    @Size(min = 1, max = 255, message = "Описание должно быть больше 1 и меньше 255 символов")
     private String description; // описание
     private Boolean available; // статус доступности
     private Long ownerId; // id владельца вещи
