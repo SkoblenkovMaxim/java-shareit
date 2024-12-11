46e95e39120074561031c02e3b2ebce2a1feb9fc
Maxim Skoblenkov <goofy.net@yandex.ru>
2024-12-12 00:26:31 +0400

    add-booking: исправлены ошибки по чекстайлу

--- src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
+++ src/main/java/ru/practicum/shareit/user/UserServiceImpl.java
@@ -69,7 +69,7 @@ public class UserServiceImpl implements UserService {
             user.setName(userDto.getName());
         }
 
-        if(userDto.getEmail() != null) {
+        if (userDto.getEmail() != null) {
             user.setEmail(userDto.getEmail());
         }
 
