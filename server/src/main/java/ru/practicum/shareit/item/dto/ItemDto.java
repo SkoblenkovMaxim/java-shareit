b27c18d170f053c17f96123df1b074e090e3c167
mskoblenkov <goofy.net@yandex.ru>
2024-12-22 20:34:47 +0400

    add-item-request-and-gateway: доработаны тест ItemDtoTest - убрана сериализация

--- server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ server/src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -4,29 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Transient;
 import ru.practicum.shareit.booking.dto.BookingShortDto;
 import ru.practicum.shareit.user.dto.UserDto;
-
-import java.io.Serializable;
 import java.util.List;
 
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class ItemDto implements Serializable {
+public class ItemDto {
     private Long id; // id вещи
     private String name; // краткое название вещи
     private String description; // описание
     private Boolean available; // статус доступности
-    @Transient
     private UserDto owner;
     private Long requestId; // id запроса вещи
-    @Transient
     private BookingShortDto lastBooking;
-    @Transient
     private BookingShortDto nextBooking;
-    @Transient
     private List<CommentDto> comments;
 }
